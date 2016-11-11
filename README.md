a219636a05cc93c4caae7931010154716afabcaa
sen <jiesenboor@gmail.com>
2016-11-11 15:59:07 +0800

    初次提交

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# validate
-表单验证插件
+# 表单验证框插件
+[在线演示](http://jiesenboor.github.io/other/demo/validate/index.html) 
+   
+###简介：
+- 这是基于`jquery`的表单验证框插件,支持验证规则扩展、自定义各验证状态的UI提示。
+- 将表单值序列化成json对象
+
+###使用方法：
+简单的手机验证框声明、并验证
+``` javascript
+var isOk = $("[name='phone']").validate("validate",{
+    required:true,
+    validType:"phone"
+});
+```
+自定义`验证函数`,验证值是否为偶数
+``` javascript
+$("[name='even']").validate({
+    trim:false,
+    required:true,
+    msg:{
+        empty:"请输入偶数",
+        invalid:"您输入了一个非偶数值"
+    },
+    validType:function(value){
+        return /^\d+$/.test(value) && (parseInt(value) & 1) === 0;
+    },
+    onEmpty:function(target,msg){
+        console.info(msg.empty);
+    },
+    onInvalid:function(target,msg){
+        target.val("");
+        console.info(msg.invalid);
+    }
+});
+```
+
+###验证规则：
+验证规则是根据使用需求和验证类型属性来定义的，这些规则已经实现：
+
+- email：匹配E-Mail的正则表达式规则。
+- url：匹配URL的正则表达式规则。
+- length[0,100]：允许在x到x之间个字符。
+- phone：配置11位手机号码。
+
+自定义验证规则，需要重写$.fn.validate.rules中定义的验证器函数。例如，定义一个最小长度(minLength)的自定义验证：
+``` javascript
+$.extend($.fn.validate.rules, {
+    minLength:function(value,param){
+        return value > param;
+    }
+});
+$("[name='username']").validate({
+    required:true,
+    msg:"用户名至少为6个字符",  //msg也可以指定任意类型
+    validType:"minLength[6]", //使用重写的验证规则
+    onInvalid:function(target,msg){
+        console.info(msg);
+    }
+});
+```
+
+###全局验证框状态样式：
+验证框有4种状态：获取焦点、验证匹配、验证匹配及失去焦点。本插件默认只实现了在验证框失去焦点时对验证框进行验证，如果不希望在失焦时进行验证动作可重写为null，其它三个状态未实现、使用时可通过重写全局的$.fn.validate.style实现各种状态的UI提示。
+``` javascript
+$.extend( $.fn.validate.style,{
+    // 验证通过时的验证框处理
+    ok:function (target,msg) {
+        target.removeClass("warn").addClass("ok");
+    },
+    // 验证不通过时的验证框处理
+    error:function (target,msg) {
+        target.removeClass("ok").addClass("warn");
+    },
+    // 获取焦点时的验证框处理
+    focus:function (target,msg) {
+        target.removeClass("ok").removeClass("warn")
+    }
+    ,
+    //如果不希望在失焦时进行验证动作可重写为null
+    focusout:null
+});
+```
+
+###属性：
+属性名     |属性值类型                |描述                                           |默认值  
+--------- |
+trim      |boolean                  |是否去掉前值后空格                               |true
+required  |boolean                  |是否必填                                        |false
+validType |function,string,string[] |验证类型，可以指定名称或直接指定方法、多个名称时用数组|null
+msg       |string,string[],{}       |消息提示对象。作为第二个参数回传给事件回调函数       |null
+
+###事件：
+事件名     | 参数            |  描述  
+--------- |
+onBefore  |target,msg       |在验证一个字段之前触发
+onEmpty   |target,msg       |在验证一个字段为空触发
+onInvalid |target,msg       |在验证一个字段为不匹配触发
+
+###事件：
+方法名     | 方法属性            |  描述  
+--------- |
+validate  |options             |验证文本框的内容是否有效
+destroy   |none                |销毁验证
---
+++ validate.js
@@ -0,0 +1,266 @@
+(function($){
+    /**
+     * 非空校验
+     * @param obj 要校验的表彰
+     * @param rule 验证规则对象
+     * @returns {boolean} 值非空返回true、否则返回false
+     */
+    var required = function (obj,rule) {
+        if(obj.val() == "") {
+            //校验失败的回调
+            rule.onEmpty && rule.onEmpty(obj,rule.msg);
+            return false;
+        }
+        return true;
+    };
+
+    /**
+     * 执行校验规则对表单值进行校验
+     * @param value 表单值
+     * @param rule 校验规则类型
+     * @returns {*} 校验通过返回true、失败返回false。找不到校验规则类型则抛出异常
+     */
+    var execute = function (value,rule) {
+        //
+        // 判断校验规则类型是否参数,则截取参数
+        // 例如:
+        //     length[5,10]
+        // 校验规则类型为:length
+        // 校验规则类型参数为:[5,10]
+        //
+        var index = rule.indexOf("[");
+        var paramArr;
+        if(index>0) {
+            paramArr = rule.substring(index+1,rule.length-1).split(",");
+            rule = rule.substr(0, index);
+        }
+
+        var method = $.fn.validate.rules[rule];
+        if(method) {
+            // 调用校验方法，如果验证类型没有参数paramArr为undefined
+            return method(value,paramArr);
+        }else{
+            // 如果找不到校验规则、则抛出异常
+            throw new Error("没有发现校验规则：" + rule);
+        }
+    };
+
+    /**
+     * 表单验证
+     * @param obj 要验证的表彰
+     * @returns {boolean} 验证通过返回
+     */
+    var validate = function (obj) {
+        //获取当前表单校验规则
+        var rule=$.data(obj,"rule");
+        //校验前回调
+        rule.onBefore && rule.onBefore(obj,rule.msg);
+
+        var value=obj.val();
+        //否则去掉表单值的前后空格
+        if(rule.trim) {
+            obj.val(value = $.trim(value));
+        }
+
+        //非空校验
+        if(rule.required && !required(obj,rule)) {
+            return false;
+        }
+
+        //如果指定了校验类型、则进行校验
+        if(rule.validType) {
+            //变量声明：校验类型、校验是否通过、当前执行校验的类型
+            var type = rule.validType,isOk,validType=undefined;
+
+            //
+            // 判断指定校验类型的方式，validType字符串、方法、字符串类型的数组
+            //
+            switch(typeof type){
+                case "function":    //如果是方法直接传递值执行
+                    isOk = type(value);
+                    break;
+                case "string":      //如果是字符串校验名称、则调用执行
+                    isOk = execute(value,type);
+                    break;
+                default:
+                    if(type instanceof Array) {
+                        //如果是校验类型数组、则遍历执行
+                        $.each(type, function (i,n) {
+                            if(!(isOk = execute(value,n))) {
+                                // 如果校验不通过、则结束校验记录当前执行的校验类型
+                                validType = n;
+                                return false;
+                            }
+                        });
+                    }else{
+                        throw new Error("校验类型validType的参数值不正确，请指定字符串类型的规则名称或直接指定校验函数，需传多个规则名称可用数组方式");
+                    }
+            }
+            if(!isOk) {
+                // 如果指定了校验不通过的回调则执行
+                rule.onInvalid && rule.onInvalid(obj,rule.msg,validType);
+                return false;
+            }
+        }
+        //默认返回true
+        return true;
+    };
+
+    /**
+     * 给表单元素绑定规则
+     * @param obj
+     * @param rule
+     */
+    var bindRule = function (obj,rule) {
+        $.data(obj, "rule", rule);
+        obj.unbind("focus", $.fn.validate.style.focus)
+        .bind(
+            "focus",
+            function () {
+                $.fn.validate.style.focus(obj,rule.msg);
+            }
+        );
+        if($.fn.validate.style.focusout) {
+            obj.unbind("focusout", $.fn.validate.style.focusout)
+            .bind(
+                "focusout",
+                function () {
+                    $.fn.validate.style.focusout(obj,rule.msg);
+                }
+            );
+        }
+    };
+
+    /**
+     * 创建表单验证
+     * @param options 验证参数
+     * @returns {*}
+     */
+    $.fn.validate = function (options) {
+        if(typeof options == "string"){
+            //如果传入了传入了两个参数、则以为第二个参数是验证规则
+            if(arguments.length==2) {
+                var opt = $.data(this,"rule") || $.fn.validate.defaults;
+                bindRule(this, $.extend({},opt,arguments[1]));
+            }
+            //执行方法
+            var method = $.fn.validate.method[options];
+            if(method) {
+                return method(this);
+            }else{
+                throw new Error("方法："+options+"未定义。可调用方法有(validate|destroy)");
+            }
+        }
+
+        // 如果传入的第一个参数不是字符串、则以为是创始验证对象
+        options = options || {};
+        bindRule(this, $.extend({},$.fn.validate.defaults,options));
+        return this;
+    };
+
+    /**
+     * 表单验证默认规则
+     * @type {{
+     *      名称         类型                      默认值    说明
+     *      trim        boolean                   true     是否去掉前后空格。默认为true
+     *      required    boolean                   false    是否必填
+     *      validType   function,string,string[]  null     验证类型，可以指定名称或直接指定方法、多个名称时用数组
+     *      msg         string,object,string[]    null     消息提示对象。作为第二个参数回传给事件回调函数
+     *      onBefore    function                  null     执行表单值验证前的回调函数
+     *      onEmpty     function                  null     验证表单值验证为空时的回调函数
+     *      onInvalid   function                  null     验证表单值验证无效时的回调函数
+     *  }}
+     */
+    $.fn.validate.defaults = {
+        trim:true,
+        required:false,
+        validType:null,
+        msg:null,
+        onBefore:null,
+        onEmpty:null,
+        onInvalid:null
+    };
+
+    /**
+     *
+     * @type {{validate: $.fn.validate.method.validate, isValid: $.fn.validate.method.isValid, destroy: $.fn.validate.method.destroy}}
+     */
+    $.fn.validate.method = {
+        validate:function (obj) {
+            var msg=$.data(obj,"rule").msg;
+            if(validate(obj)) {
+                //执行显示校验成功的样式处理方法
+                $.fn.validate.style.ok(obj,msg);
+                return true;
+            }
+            //执行显示校验失败的样式处理方法
+            $.fn.validate.style.error(obj,msg);
+            return false;
+        },
+        destroy:function (obj) {
+            $.data(obj,"rule",undefined);
+        }
+    };
+
+    /**
+     * 表单验证的全局样式。
+     * 扩展方式：
+     *      $.extend($.fn.validate.style,{});
+     * @type {{ok: $.fn.validate.style.ok, error: $.fn.validate.style.error, focus: $.fn.validate.style.focus}}
+     */
+    $.fn.validate.style = {
+        ok:function () {},
+        error:function () {},
+        focus:function () {},
+        focusout:function (obj) {
+            $.fn.validate.method.validate(obj);
+        }
+    };
+
+    /**
+     * 验证规则
+     * @type {{phone: $.fn.validate.rules.phone}}
+     */
+    $.fn.validate.rules = {
+        phone:function (value) {
+            return /^1(3|4|5|7|8)\d{9}$/.test(value);
+        },
+        sms_code:function (value) {
+            return /^\d{6}$/.test(value);
+        },
+        img_code:function (value) {
+            return /^[A-Za-z0-9]{4}$/.test(value);
+        },
+        email:function (value) {
+            return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value);
+        },
+        realname:function (value) {
+            return /^[\u4e00-\u9fa5]{2,6}$/.test(value);
+        },
+        url:function (value) {
+            return /^https?:\/\/([\dA-Za-z\.-]+)\.([A-Za-z\.]{2,6})([\/\w \.-]*)*\/?(\?.+)?$/.test(value);
+        },
+        length:function(value,param){
+            return value >= param[0] && value <= param[1];
+        }
+    };
+
+    /**
+     * 将表单值序列化成对象
+     * @returns {{}}
+     */
+    $.fn.formToJson = function () {
+        if(!this.is("form")) {
+            throw new Error("当前元素非form表单元素");
+        }
+        var obj = {};
+        $.each(this.serializeArray(), function() {
+            if (obj[this['name']]) {
+                obj[this['name']] = obj[this['name']] + "," + this['value'];
+            } else {
+                obj[this['name']] = this['value'];
+            }
+        });
+        return obj;
+    };
+})(jQuery);
